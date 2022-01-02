97753aa312bfffe08600aa93c3b0401e6613a1ae
Nimouh <simaoaugusto11@hotmail.com>
2022-01-02 10:09:41 +0000

    teste

--- src/Conectar.java
+++ src/Conectar.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class Conectar {
     public static Connection getCon() {
         try {
-            String url = "jdbc:sqlserver://DESKTOP-UJ26N4k\\SQLEXPRESS;databaseName=ZurrapaFilial;integratedSecurity=true";
+            String url = "jdbc:sqlserver://NIMOUH-BEAST\\SQLEXPRESS;databaseName=ZurrapaFilial;integratedSecurity=true";
             Connection connection = DriverManager.getConnection(url);
             return connection;
         } catch (Exception e) {
@@ -14,7 +14,7 @@ public class Conectar {
     //ainda nao foi utilizada
         public static Connection getConSede() {
             try {
-                String url = "jdbc:sqlserver://DESKTOP-UJ26N4k\\SQLEXPRESS;databaseName=ZurrapaSede;integratedSecurity=true";
+                String url = "jdbc:sqlserver://NIMOUH-BEAST\\SQLEXPRESS;databaseName=ZurrapaSede;integratedSecurity=true";
                 Connection connection = DriverManager.getConnection(url);
                 return connection;
             } catch (Exception e) {
